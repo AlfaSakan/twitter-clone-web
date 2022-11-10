4a554b380eaa5fe2d30237fe7bb87680e38c8753
alfa sakan <ahmadsakan@ist.id>
2022-11-10 15:05:33 +0700

    fix: env key name

--- src/contants/env.ts
+++ src/contants/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-  apiBaseUrl: import.meta.env.API_BASE_URL ?? "http://localhost:8081",
-  apiVersion: import.meta.env.API_VERSION || "/v1",
+  apiBaseUrl: import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8081",
+  apiVersion: import.meta.env.VITE_API_VERSION || "/v1",
 };
